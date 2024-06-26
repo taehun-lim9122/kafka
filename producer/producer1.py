b6a36e95f0990ec5557c61c6dbd0bb2705ed7f51
taehun-lim9122 <lth9122@gmail.com>
2024-06-26 19:37:18 +0900

    broker,zookeeper 와 producer, connect, setup docker compose 분리

--- producer/producer1.py
+++ producer/producer1.py
@@ -1,19 +1,22 @@
 from kafka import KafkaProducer
 from json import dumps
 import time
+import os
+
+bootstrap_servers = 'broker:29092'
 
 producer = KafkaProducer(
     acks=0, # 메시지 전송 완료에 대한 체크
     compression_type='gzip', # 메시지 전달할 때 압축(None, gzip, snappy, lz4 등)
-    bootstrap_servers=['localhost:9092'], # 전달하고자 하는 카프카 브로커의 주소 리스트
+    bootstrap_servers= bootstrap_servers, # 전달하고자 하는 카프카 브로커의 주소 리스트
     value_serializer=lambda x:dumps(x).encode('utf-8') # 메시지의 값 직렬화
 )
 
 start = time.time()
 
 for i in range(100):
-    data = {'test' : 'kor'+str(i)}
-    producer.send('test-1', value=data)
+    data = {'producer3' : 'test'+str(i)}
+    producer.send('producer1', value=data)
     producer.flush() # 
-
+    
 print('[Done]:', time.time() - start)
